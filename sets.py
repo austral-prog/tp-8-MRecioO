dc393ee6b5ebb4c81d6624096827637b3c389d05
MRecioO <162721939+MRecioO@users.noreply.github.com>
2024-05-24 19:47:34 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -5,16 +5,16 @@ def clean_ingredients(dish_name, dish_ingredients):
     return [dish_name, dish_ingredients]
 
 def check_drinks(drink_name, drink_ingredients):
+    x = 0
     for ing in drink_ingredients:
-        ing = drink_ingredients[0]
+        ing = drink_ingredients[0+x]
         if ing in ALCOHOLS:
             tail_type = 'Cocktail'
             break
         else:
-            drink_ingredients.pop(drink_ingredients.index(ing))
             tail_type = 'Mocktail'
-            if len(drink_ingredients)-1 > 0 :
-                next
+            if x < len(drink_ingredients)-1:
+                x += 1
             else:
                 break
     return drink_name +' '+ tail_type
