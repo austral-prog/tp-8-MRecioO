4286907c01dd8d0e14dd6406b2b22da155ac00ab
MRecioO <162721939+MRecioO@users.noreply.github.com>
2024-05-24 17:37:38 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,15 +1,20 @@
 from sets_categories_data import (ALCOHOLS)
 
-
 def clean_ingredients(dish_name, dish_ingredients):
     dish_ingredients = set(dish_ingredients)
     return [dish_name, dish_ingredients]
 
 def check_drinks(drink_name, drink_ingredients):
     x = 0
-    ing = drink_ingredients[0+x]
-    while ing not in ALCOHOLS:
-        x += 1
-        return f'{drink_name} Mocktail'
-    else:
-        return f'{drink_name} Cocktail'
+    for ing in ALCOHOLS:
+        ing = drink_ingredients[0+x]
+        if ing in ALCOHOLS:
+            tail_type = 'Cocktail'
+            break
+        else:
+            tail_type = 'Mocktail'
+            if x < len(drink_ingredients)-1:
+                x += 1
+            else:
+                break
+    return drink_name +' '+ tail_type
