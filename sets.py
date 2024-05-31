255aa65c3abc7c65a87a426d46adf5d62e35b289
MRecioO <162721939+MRecioO@users.noreply.github.com>
2024-05-30 23:40:04 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -5,11 +5,15 @@ def clean_ingredients(dish_name, dish_ingredients):
     return [dish_name, dish_ingredients]
 
 def check_drinks(drink_name, drink_ingredients):
-    ing_set = set(drink_ingredients)
-
-    if ing_set.intersection(ALCOHOLS) != set():
-        tail_type = 'Cocktail'
-        return drink_name +' '+ tail_type
-    else:
-        tail_type = 'Mocktail'
-        return drink_name +' '+ tail_type
+    for ing in drink_ingredients:
+        x = 0
+        if ing in ALCOHOLS:
+            tail_type = 'Cocktail'
+            break
+        else:
+            tail_type = 'Mocktail'
+            if x < len(drink_ingredients)-1:
+                x += 1
+            else:
+                break
+    return drink_name +' '+ tail_type
